3e137b96b8afda93f9dbedb90db6d2c34962b70d
Slavo Kozar <slavo.kozar@gmail.com>
2023-04-06 21:11:41 +0200

    adding task

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './style.css';
+import {useState} from "react";
 
 export default function App() {
-    function handleSubmit(e) {
-        e.preventDefault();
-
-        console.log("submit form");
-    }
 
     const initTasks = [
         {
-            text: "Completed Task",
+            text: "Completed Task 12312312",
             completed: true,
         },
         {
@@ -18,6 +14,33 @@ export default function App() {
         }
     ];
 
+    const [tasks, setTasks] = useState(initTasks);
+    const [input, setInput] = useState('');
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        // const newTask = {
+        //     text: input,
+        //     completed: false
+        // }
+        //
+        // const newTasksValue = tasks.concat(newTask);
+        //
+        // setTasks(newTasksValue);
+
+        setTasks(
+            tasks.concat(
+                {
+                    text: input,
+                    completed: false
+                }
+            )
+        )
+
+        console.log("submit form");
+    }
+
     return (
         <div className="container py-5">
             <div className="card">
@@ -25,6 +48,10 @@ export default function App() {
                     <h1 className="card-title text-center mb-5">Awesome Todo list</h1>
                     <form onSubmit={handleSubmit} className="add-items d-flex mb-3">
                         <input
+                            value={input}
+                            onChange={(event) => {
+                                setInput(event.target.value)
+                            }}
                             type="text"
                             className="form-control me-2"
                             placeholder="What do you need to do today?"
@@ -51,52 +78,42 @@ export default function App() {
                         {/* unordered list */}
                         <ul>
                             {/* list item */}
-                            <li className="py-2 d-flex justify-content-between completed">
-                                <div className="form-check">
-                                    <label className="form-check-label">
-                                        <input className="form-check-input" type="checkbox"/>
-                                        Completed Task
-                                    </label>
-                                </div>
-                                <button
-                                    type="button"
-                                    className="btn btn-sm btn-link text-danger"
-                                >
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                         className="bi bi-trash" viewBox="0 0 16 16">
-                                        <path
-                                            d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
-                                        <path fillRule="evenodd"
-                                              d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
-                                    </svg>
-                                </button>
-                            </li>
-                            <li className="py-2 d-flex justify-content-between">
-                                <div className="form-check">
-                                    <label className="form-check-label">
-                                        <input className="form-check-input" type="checkbox"/>
-                                        Todo Task
-                                    </label>
-                                </div>
-                                <button
-                                    type="button"
-                                    className="btn btn-sm btn-link text-danger"
-                                >
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                         className="bi bi-trash" viewBox="0 0 16 16">
-                                        <path
-                                            d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
-                                        <path fillRule="evenodd"
-                                              d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
-                                    </svg>
-                                </button>
-                            </li>
+
+                            {
+                                tasks.map((task, index) => {
+                                    console.log(task, index);
+
+                                    return (
+                                        <li key={index} className="py-2 d-flex justify-content-between completed">
+                                            <div className="form-check">
+                                                <label className="form-check-label">
+                                                    <input className="form-check-input" type="checkbox"/>
+                                                    {task.text}
+                                                </label>
+                                            </div>
+                                            <button
+                                                type="button"
+                                                className="btn btn-sm btn-link text-danger"
+                                            >
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                     fill="currentColor"
+                                                     className="bi bi-trash" viewBox="0 0 16 16">
+                                                    <path
+                                                        d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
+                                                    <path fillRule="evenodd"
+                                                          d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
+                                                </svg>
+                                            </button>
+                                        </li>
+                                    );
+                                })
+                            }
                         </ul>
                     </div>
                     {/*<p className="">No Tasks for today :) </p>*/}
                 </div>
 
-                <div class="card-footer text-center">
+                <div className="card-footer text-center">
                     &copy; 2022
                 </div>
             </div>
