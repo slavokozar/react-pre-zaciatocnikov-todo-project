48702f257cc5b174dd4caef585b890dd72a747b0
Slavo Kozar <slavo.kozar@gmail.com>
2024-04-10 17:35:03 +0200

    created Task component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './style.css';
 import {useState} from "react";
+import Task from "./Task";
 
 export default function App() {
 
@@ -80,36 +81,10 @@ export default function App() {
                                     {
                                         tasks.map(
                                             (task, index) => (
-                                                //  className="completed"
-                                                <li key={index} className="py-2 d-flex justify-content-between">
-                                                    <div className="form-check">
-                                                        <label className="form-check-label">
-                                                            <input className="form-check-input" type="checkbox"/>
-                                                            {task.text}
-                                                        </label>
-                                                    </div>
-                                                    <button
-                                                        type="button"
-                                                        className="btn btn-sm btn-link text-danger"
-                                                        onClick={() => {
-                                                            setTasks(
-                                                                tasks.filter((t, i) =>
-                                                                    index !== i
-                                                                )
-                                                            )
-                                                        }}
-                                                    >
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16"
-                                                             height="16"
-                                                             fill="currentColor" className="bi bi-trash"
-                                                             viewBox="0 0 16 16">
-                                                            <path
-                                                                d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
-                                                            <path fillRule="evenodd"
-                                                                  d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
-                                                        </svg>
-                                                    </button>
-                                                </li>
+                                                <Task
+                                                    key={index}
+                                                    text={task.text}
+                                                />
                                             )
                                         )
                                     }
